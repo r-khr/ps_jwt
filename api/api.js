6ff4e3c81f21f0139f9c7c9ef12510b10c78259a
Roman Khrystynych <roman.khrystynych@gmail.com>
2015-12-09 00:18:48 -0500

    updated to use passport for login and register

--- api/api.js
+++ api/api.js
@@ -3,10 +3,17 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var User = require('./models/User.js');
 var jwt = require('jwt-simple');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var app = express();
 
 app.use(bodyParser.json());
+app.use(passport.initialize());
+
+passport.serializeUser(function(user, done){
+	done(null, user.id);
+})
 
 app.use(function(req, res, next){
 	res.header('Access-Control-Allow-Origin', '*');
@@ -16,41 +23,55 @@ app.use(function(req, res, next){
 	next();
 });
 
-app.post('/register', function(req, res){
-	var user = req.body;
+var strategyOptions = {usernameField: 'email'};
 
-	var newUser = new User({
-		email: user.email,
-		password: user.password
-	});
+var loginStrategy = new LocalStrategy(strategyOptions, 
+	function(email, password, done){
+		var searchUser = {
+			email: email
+		};
 
-	newUser.save(function(err){
-		createSendToken(newUser, res);
-	})
+		User.findOne(searchUser, function(err, user){
+			if (err) return done(err);
+
+			if(!user) return done(null, false, {
+				message: 'Wrong email/password'
+			});
+
+			user.comparePasswords(password, function(err, isMatch){
+				if (err) return done(err);
+
+				if(!isMatch) return done(null, false, { 
+					message: 'Wrong email/password'
+				});
+				
+				return done(null, user);
+			});
+		})
 });
 
-app.post('/login', function(req, res){
-	req.user = req.body;
-
-	var searchUser = {
-		email: req.user.email
-	};
-
-	User.findOne(searchUser, function(err, user){
-		if (err) throw err;
-
-		if(!user)
-			return res.status(401).send({message: 'Wrong email/password'});
-
-		user.comparePasswords(req.user.password, function(err, isMatch){
-			if(err) throw err;
-
-			if(!isMatch)
-				return res.status(401).send({message: 'Wrong email/password'});
-			
-			createSendToken(user, res);
+var registerStrategy = new LocalStrategy(strategyOptions, 
+	function(email, password, done){
+		var newUser = new User({
+			email: email,
+			password: password
 		});
-	})
+
+		newUser.save(function(err){
+			done(null, newUser);
+		})
+
+});
+
+passport.use('local-register', registerStrategy);
+passport.use('local-login', loginStrategy);
+
+app.post('/register', passport.authenticate('local-register'), function(req, res){
+	createSendToken(req.user, res);
+});
+
+app.post('/login', passport.authenticate('local-login'), function(req, res){
+	createSendToken(req.user, res);
 })
 
 function createSendToken(user, res){
