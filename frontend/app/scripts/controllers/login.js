7805cf7117026ede0fe2aa9e0c709c189900ff2e
Roman Khrystynych <romankhrystynych@Romans-MacBook-Pro.local>
2015-12-17 23:13:36 -0500

    updating login to have oauth

--- frontend/app/scripts/controllers/login.js
+++ frontend/app/scripts/controllers/login.js
@@ -2,14 +2,19 @@
 
 angular.module('psJwtApp')
 .controller('LoginCtrl', function ($scope, alert, auth) {
-	$scope.submit = function(){
+	$scope.submit = function() {
 
 		auth.login($scope.email, $scope.password)
 			.success(function(res){
-				alert('success', 'Welcome', 'Thanks for coming back ' + res.user.email + '!');
+				alert('success', 'Welcome, ', ' Thanks for coming back ' + res.user.email + '!');
 			})
 			.error(function(err){
 				alert('warning', 'Something went wrong :( ', err.message);
 			});
-	};
+	}
+
+	$scope.google = function() {
+		auth.googleAuth().then();
+	}
+
 });
